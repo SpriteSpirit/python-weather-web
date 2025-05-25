589e08d42b830318491cb085dc0308af75a65403
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-05-25 20:08:11 +0300

    Реализована функция для получения координат по названию города

--- src/weather_app/services.py
+++ src/weather_app/services.py
@@ -5,6 +5,8 @@ import os
 
 from django.test import AsyncClient
 
+from weather_app.models import City
+
 access_key = os.environ.get('API_KEY')
 
 
@@ -39,3 +41,20 @@ async def get_yandex_weather(lat: float, lon: float) -> Optional[dict]:
         except requests.HTTPError as e:
             print(f'Ошибка запроса к API Яндекс.Погода: {e}')
             return None
+
+
+def get_city_coordinates_by_name(city_name: str) -> Optional[Tuple[float, float]]:
+    """
+    Получение координат из модели города City
+
+    :param city_name: Название города
+    :return: Кортеж из широты и долготы или None в случае ошибки
+    """
+
+    try:
+        city = City.objects.get(name=city_name)
+        lat, lon = map(float, city.location.split(','))
+
+        return lat, lon
+    except (City.DoesNotExist, ValueError, AttributeError):
+        return None
