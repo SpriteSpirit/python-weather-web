334a90b34975e30995e2240c2e0ac33879ebbf9b
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-05-25 20:03:20 +0300

    Реализована модель города

--- src/weather_app/models.py
+++ src/weather_app/models.py
@@ -1,8 +1,21 @@
 # Create your models here.
+
 from django.db import models
 from location_field.models.plain import PlainLocationField
 
+NULLABLE = {'null': True, 'blank': True}
 
-class Place(models.Model):
-    city = models.CharField(max_length=255)
-    location = PlainLocationField(based_fields=['city'], zoom=7)
+
+class City(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название города')
+    location = PlainLocationField(
+        based_fields=['name'],
+        zoom=7,
+        verbose_name="Город на карте")
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
