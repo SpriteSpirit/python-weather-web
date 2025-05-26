ce8b2b36d3f6adf94e6c512b8c4e296fc86212c9
SpriteSpirit <Sprite.spirit.t.me@gmail.com>
2025-05-27 01:34:49 +0300

    Реализована модель для хранения истории поиска

--- src/weather_app/models.py
+++ src/weather_app/models.py
@@ -1,21 +1,35 @@
-# Create your models here.
-
 from django.db import models
-from location_field.models.plain import PlainLocationField
 
-NULLABLE = {'null': True, 'blank': True}
+NULLABLE = {"null": True, "blank": True}
 
 
-class City(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Название города')
-    location = PlainLocationField(
-        based_fields=['name'],
-        zoom=7,
-        verbose_name="Город на карте")
+class SearchHistory(models.Model):
+    """
+    Модель для хранения истории поиска погоды по городам.
+    """
+
+    # Если нужно привязать к пользователю:
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE, verbose_name="Пользователь")
+
+    city = models.CharField(max_length=100, verbose_name="Название населенного пункта")
+    latitude = models.FloatField(verbose_name="Широта", **NULLABLE)
+    longitude = models.FloatField(verbose_name="Долгота", **NULLABLE)
+    search_count = models.PositiveIntegerField(default=1, verbose_name="Количество запросов")
+    last_searched = models.DateTimeField(auto_now=True, verbose_name="Последний поиск")
 
     class Meta:
-        verbose_name = 'Город'
-        verbose_name_plural = 'Города'
+        """
+        Дополнительные настройки модели.
+        """
+
+        verbose_name = "Запись истории поиска"
+        verbose_name_plural = "История поисковых запросов"
+        unique_together = ("city",)
+        ordering = ["-last_searched"]
+        indexes = [
+            models.Index(fields=["city"]),
+            models.Index(fields=["last_searched"]),
+        ]
 
     def __str__(self):
-        return self.name
+        return f"{self.city} (найден {self.search_count} раз)"
